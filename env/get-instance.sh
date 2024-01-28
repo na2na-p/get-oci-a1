11c191285e2026a9ec53913bbf79f63e8ed0868e
na2na-p <na2na@na2na.dev>
2024-01-29 02:03:32 +0900

    自動周回スクリプト改修

--- env/get-instance.sh
+++ env/get-instance.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 count=0
 while true; do
   ((count++))
   echo "try $count"
-  terraform apply -auto-approve
-  if [ $? -ne 1 ]; then
+
+  # Terraform applyを実行し、出力を表示
+  output=$(terraform apply -auto-approve 2>&1)
+  echo "$output"
+
+  if [[ $output == *"500-InternalError, Out of host capacity."* ]]; then
+    echo "Encountered 'Out of host capacity' error, not logging this error."
+  elif [[ $output == *"Error:"* ]] || [[ $output == *"error:"* ]]; then
+    clean_output=$(echo "$output" | sed 's/\x1b\[[0-9;]*m//g')
+    echo "Error on try $count: $clean_output" >> result.txt
+  elif [ $exit_code -eq 0 ]; then
     echo "Resource Created!" >> result.txt
     exit
   fi
+
   sleep 10
 done
